846add7e9a8c8a43ed00d39d2606577f4ee3ce2f
vivek213ranjan <ranjanvivek1994@gmail.com>
2017-06-18 10:54:34 +0530

    added popular component with css and update language functinoality

--- app/index.js
+++ app/index.js
@@ -1,20 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
-
-//properties associated with the component
-//state
-//lifecycle event
-//UI
-
-class App extends React.Component {
-	render() {
-		return (
-			<div>
-				Hello World!
-			</div>
-		)
-	}
-}
+var App = require('./components/App');
 
 ReactDOM.render(<App />, document.getElementById("app"));
